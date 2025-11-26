4667192ff0a3ba0cdc30485bd8f73ad1509146a2
Angel Sanjay MC <angsan.kernel@gmail.com>
2025-11-26 07:37:36 -0600

    Add files via upload

---
+++ SalusCMA/API/coneccion.php
@@ -0,0 +1,12 @@
+<?php
+    $host ='localhost';
+    $db = '';
+    $user = '';
+    $pass = '';
+
+    $con = new mysqli($host, $user, $pass, $db);
+
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    }  
+?>
